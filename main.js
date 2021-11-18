9574d041404bcf6ff48332603b771aaf46c038bc
Vidar Rehn <vidar.rehn@gmail.com>
2021-11-18 12:39:18 +0100

    third commit. added JS-file with servings-functionality

--- main.js
+++ main.js
@@ -1,21 +1,89 @@
 
-let spaghettiCarbonara = document.querySelector("#carbonara");
-let gemelliFunghi = document.querySelector("#funghi");
-let papardelleRagu = document.querySelector("#ragu");
+let carbonaraServingsInput = document.querySelectorAll("select")[0];
+let raguServingsInput = document.querySelectorAll("select")[1];
+let funghiServingsInput = document.querySelectorAll("select")[2];
 
-let nextButton = document.querySelector(".next-btn");
+let carbonaraAmounts = document.querySelectorAll("#carbonara .amount");
+let raguAmounts = document.querySelectorAll("#ragu .amount");
+let funghiAmounts = document.querySelectorAll("#funghi .amount");
 
-function nextDish(){
-    if (spaghettiCarbonara.classList.contains("hide") == false){
-        spaghettiCarbonara.classList.add("hide");
-        gemelliFunghi.classList.remove("hide");
-    } else if (gemelliFunghi.classList.contains("hide") == false){
-        gemelliFunghi.classList.add("hide");
-        papardelleRagu.classList.remove("hide");
-    } else {
-        papardelleRagu.classList.add("hide");
-        spaghettiCarbonara.classList.remove("hide");
+const carbonaraOriginalAmounts = [];
+carbonaraAmounts.forEach((amount) =>{
+    carbonaraOriginalAmounts.push(parseInt(amount.innerText));
+})
+
+const raguOriginalAmounts = [];
+raguAmounts.forEach((amount) =>{
+    raguOriginalAmounts.push(parseInt(amount.innerText));
+})
+
+const funghiOriginalAmounts = [];
+funghiAmounts.forEach((amount) =>{
+    funghiOriginalAmounts.push(parseInt(amount.innerText));
+})
+
+let carbonaraOptions = carbonaraServingsInput.querySelectorAll("option");
+let raguOptions = raguServingsInput.querySelectorAll("option");
+let funghiOptions = funghiServingsInput.querySelectorAll("option");
+
+
+carbonaraServingsInput.addEventListener("change", ()=>{
+    if (carbonaraServingsInput.value == 2){
+        carbonaraAmounts.forEach((amount, index)=>{
+            amount.innerText = `${(carbonaraOriginalAmounts[index])*0.5} `;
+        })
+    } else if (carbonaraServingsInput.value == 4){
+        carbonaraAmounts.forEach((amount, index)=>{
+            amount.innerText = `${(carbonaraOriginalAmounts[index])*1} `;
+        })
+    } else if (carbonaraServingsInput.value == 6){
+        carbonaraAmounts.forEach((amount, index)=>{
+            amount.innerText = `${(carbonaraOriginalAmounts[index])*1.5} `;
+        })
+    } else if (carbonaraServingsInput.value == 8){
+        carbonaraAmounts.forEach((amount, index)=>{
+            amount.innerText = `${(carbonaraOriginalAmounts[index])*2} `;
+        })
     }
-}
+})
 
-nextButton.addEventListener("click", nextDish);
+raguServingsInput.addEventListener("change", ()=>{
+    if (raguServingsInput.value == 2){
+        raguAmounts.forEach((amount, index)=>{
+            amount.innerText = `${(raguOriginalAmounts[index])*0.5} `;
+        })
+    } else if (raguServingsInput.value == 4){
+        raguAmounts.forEach((amount, index)=>{
+            amount.innerText = `${(raguOriginalAmounts[index])*1} `;
+        })
+    } else if (raguServingsInput.value == 6){
+        raguAmounts.forEach((amount, index)=>{
+            amount.innerText = `${(raguOriginalAmounts[index])*1.5} `;
+        })
+    } else if (raguServingsInput.value == 8){
+        raguAmounts.forEach((amount, index)=>{
+            amount.innerText = `${(raguOriginalAmounts[index])*2} `;
+        })
+    }
+})
+
+funghiServingsInput.addEventListener("change", ()=>{
+    if (funghiServingsInput.value == 2){
+        funghiAmounts.forEach((amount, index)=>{
+            amount.innerText = `${(funghiOriginalAmounts[index])*0.5} `;
+        })
+    } else if (funghiServingsInput.value == 4){
+        funghiAmounts.forEach((amount, index)=>{
+            amount.innerText = `${(funghiOriginalAmounts[index])*1} `;
+        })
+    } else if (funghiServingsInput.value == 6){
+        funghiAmounts.forEach((amount, index)=>{
+            amount.innerText = `${(funghiOriginalAmounts[index])*1.5} `;
+        })
+    } else if (funghiServingsInput.value == 8){
+        funghiAmounts.forEach((amount, index)=>{
+            amount.innerText = `${(funghiOriginalAmounts[index])*2} `;
+        })
+    }
+})
+
